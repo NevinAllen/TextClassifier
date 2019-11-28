91175be340f8d293470c3847e58eb0fdf4102610
NevinAllen <53032033+NevinAllen@users.noreply.github.com>
2019-11-28 09:46:33 +0800

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -14,10 +14,13 @@ function analyze() {
       var response = JSON.parse(e.target.responseText);
       if (response["result"] == "negative") {
         el("result-label").innerHTML = `That's not nice.`;
+        el("result-label").innerHTML = "Rating: " + average ;
       } else if (response["result"] == "neutral") {
         el("result-label").innerHTML = `Okay..`;
+        el("result-label").innerHTML = "Rating: " + average ;
       } else if (response["result"] == "positive") {
         el("result-label").innerHTML = `Keep it up!`;
+        el("result-label").innerHTML = "Rating: " + average ;
       } else {
         el("result-label").innerHTML = `${response["result"]}`;
       }
